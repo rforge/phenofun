1cab6dacbee243e05d73187359919030b358c36f
ludwigmax <ludwigmax@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-30 10:24:54 +0000

    sample code added and version comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phenofun@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ sample_code/phenofun_sample_analysis.R
@@ -0,0 +1,131 @@
+##############################################
+# Package phenofun
+# sample code for uroi analysis
+# part 2: uroi analysis 
+#
+# Date: 03.03.16
+# Author: Ludwig Bothmann
+##############################################
+
+#################################################################
+#	0. Set parameters
+#################################################################
+
+rm(list=ls())
+
+setwd("/home/bothmannlu/Dokumente/Phenology")
+
+library(devtools)
+load_all("rcode/phenofun")
+
+# setwd("/home/bothmannlu/Dokumente/Phenology/rcode")
+# install.packages("rcode/phenofun_0.0.4.tar.gz", repos=NULL, type="source")
+# library(phenofun)
+# setwd("/home/bothmannlu/Dokumente/Phenology")
+
+
+# source file with parameters
+filename_parameters <- "rcode/phenofun_sample_parameters.R"
+
+source(filename_parameters)
+
+# create directories if not done manually
+create_dirs_uroi(path_base=path_base,
+					  name_of_analysis=name_of_analysis)
+
+#################################################################
+#	1. Find clusters in the images
+#################################################################
+
+clusters_out <- find_clusters(folder=folder, 
+										lists_files=lists_files, 
+										which_images=which_images, 
+										n_pc_vec=n_pc_vec, 
+										k_vec=k_vec, 
+										path_base=path_base, 
+										name_of_analysis=name_of_analysis,
+										save_results=TRUE
+										# ,colormode="Grayscale"
+										,x=x,y=y
+										)
+
+#################################################################
+#	2. Generate masks
+#################################################################
+
+masks_list <- generate_masks(settings=clusters_out$settings,
+									  cluster_list=clusters_out$cluster_list,
+									  name_of_analysis=name_of_analysis,
+									  path_base=path_base,
+									  return_masks = TRUE,
+									  save_masks=TRUE,
+									  type=".png")
+
+# Overlay of masks with reference image
+# Specify reference image
+ref_image <- readImage(paste0(folder,lists_files[1]))
+ref_image <- ref_image[x,y,]
+# Compute overlay
+overlay <- overlay_masks_onref(masks_list, ref_image, alpha=.3)
+display(overlay)
+
+# and save overlay
+writeImage(overlay,files=paste0(path_base,name_of_analysis,"/masks/overlay.jpg"))
+
+####################################################################
+#	3. Mask images and compute mean %greenness time series (per doy)
+####################################################################
+
+if(do_mean_greenness){
+	
+	mean_greenness <- compute_greenness_time_series(color="green",
+																	which_images=which_images,
+																	doy=doy,
+																	path_base=path_base,
+																	name_of_analysis=name_of_analysis,
+																	main_plot=main_plot,
+																	settings_mat=clusters_out$settings_mat,
+																	load_masks=FALSE,
+																	masks_list=masks_list)
+}
+
+#################################################################
+#	4. Analysis of structural changes
+#################################################################
+
+# Note: you need more data than the example data to carry out an analysis of
+#	structural changes
+
+if(do_strucchange){
+	
+	# take \%greenness from step 3
+	mean_doy_norm <- mean_greenness$mean_doy_norm
+	settings_mat <- mean_greenness$settings_mat
+	
+	# # or load it
+	# load("results/lackenberg/mask_timeseries/uroi/160130_2014_4_gutebilder/mean_doys_uROI_green.RData")
+	# load("results/lackenberg/160201/mask_timeseries/mean_doys_uROI_green.RData")
+	# # Results paper:
+	# load("results/kranzberg2/comparison_rel_green/mask_timeseries/151013_mean_doys_cluster_green.RData")
+	# mean_doy_norm <- rbind(mean_doy1, mean_doy2, mean_doy3)
+	
+	# Possible spring doys
+	a_vec <- seq(90,150,by=1)
+	# Possible autumn doys (counted backwards from 31.12.)
+	b_vec <- seq(30,100,by=1)
+	
+	# With optimality criterion 1
+	ftests <- struc_ftest(mean_doy_norm, uROI=TRUE)
+	
+	# With optimality criterion 2
+	max_corr <- struc_dummy(mean_doy_norm, a_vec, b_vec, doy=NULL)
+	
+	# check mean_greenness$settings_mat to know which mask corresponds to which setting
+	settings_mat[ftests$wm1,]
+	settings_mat[ftests$wmx,]
+	settings_mat[max_corr$wm1,]
+	# settings_mat	
+}
+
+sessionInfo()
+Sys.time()
---
+++ sample_code/phenofun_sample_parameters.R
@@ -0,0 +1,72 @@
+##############################################
+# Package phenofun
+# sample code for uroi analysis
+# part 1: parameter setting
+# 
+# Date: 30.05.16
+# Author: Ludwig Bothmann
+##############################################
+
+do_strucchange <- FALSE
+do_mean_greenness <- FALSE
+
+# Name of analysis (perhaps with year of used data)
+# => Only for file names of saved objects, can be anything
+name_of_analysis <- "160530"
+
+# Which images shall be analysed
+complete_year <- FALSE
+year_analysis <- 2014 # only relevant if complete_year=TRUE
+n_images_try <- 10 # 834 # only relevant if complete_year=FALSE
+
+# Parameters for the clusteranalysis
+n_pc_vec <- 2#c(12,24)# c(4,8,12,16,20) #8 #c(12,24)#2 # c(12,36) # NULL
+k_vec <- 2:3#4:15#4:6
+
+# Iterations of kmeans
+# nstart <- 1 #2
+set.seed(1112)
+
+# Base directory for the results (has to exist)
+path_base <- "../../Phenology/results/phenofun_test/"
+
+# Directory containing the images
+folder <- "../../Phenology/data/WebCam/cam7/"
+
+# Vector with file names of relevant images
+lists_files <- list.files(path=folder)[-(1:8)]
+
+# Extract year and doy from file name...
+year <- as.numeric(substr(lists_files,12,15))
+doy <- as.numeric(substr(lists_files,26,28))
+
+# # ... or specify it directly, for example:
+# year <- rep(year_analysis, length(lists_files))
+# doy <- 1:365
+
+# Title for plot
+main_plot <- paste0("Kranzberg 2 - ",year_analysis)
+
+# Pixels of the images which shall be considered 
+#	(x- and y-coordinates, only rectangles possible by now)
+# If not specified: Whole image is analysed
+x <- 1285:2560
+y <- 1:960
+
+if(complete_year){
+	
+	# Only images of year "year_analysis"
+	which_images <- seq_len(length(lists_files))[which(year%in%year_analysis)]
+	
+}else{
+	
+	# Only first n_images_try images in the folder
+	which_images <- seq_len(n_images_try)	
+}
+
+# # you can define which_images in any way, for example for 50 images:
+# which_images <- round(seq(1,length(lists_files), length=50))
+# # ... or for each 10. image:
+# which_images <- round(seq(1,length(lists_files), by=10))
+
+# which_images <- which_images[seq(1,length(which_images), length=100)]
